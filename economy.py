2734ac7f0f05e38f053ee42cc31b3251c40dabb6
Pythonwolf7734 <82569523+Pythonwolf7734@users.noreply.github.com>
2021-09-13 15:17:46 +0200

    Moved commands to cog
    Added a cog for all economy commands, moved classes and database to this cog

---
+++ economy.py
@@ -0,0 +1,147 @@
+import discord
+from discord_slash import cog_ext, SlashContext
+from discord.ext import commands
+from discord_slash.utils.manage_commands import create_option, create_choice
+from discord_slash.utils.manage_components import create_button, create_actionrow
+from discord_slash.model import ButtonStyle
+import sqlite3
+
+#Utility functions
+def read_replace(read, type="db"):
+    #read = str(read)
+    if type != "db":
+        read=str(read)
+    if type == "db":
+        if read is None:
+            return None
+        for r in read:
+            read = r
+    elif type == "userid":
+        read = read.replace(">", "")
+        read = read.replace("<@", "")
+        read = read.replace("!", "")
+    elif type == "channelid":
+        read = read.replace("<#", "")
+        read = read.replace(">", "")
+    elif type == "roleid":
+        read = read.replace("<", "")
+        read = read.replace("@", "")
+        read = read.replace("&", "")
+        read = read.replace(">", "")
+    return read
+#End utility functions
+
+
+
+#DATABASE CREATE
+connection = sqlite3.connect("/media/pi/JONAH_3/Python/Discord_Bot/Regin/Reggin_data.db") #Opening Database
+cursor = connection.cursor()
+
+#cursor.execute("""CREATE TABLE users (
+#        userid INT, ing_name TEXT, clan INT, money INT, lastDaily TEXT, attack INT, health INT, defend INT
+#    )""")   #Creating table with data for users
+
+#cursor.execute("DROP TABLE clans")
+
+#cursor.execute("""CREATE TABLE clans (
+#        clanid INT, public INT, owner INT, level INT, name TEXT, castle INT
+#    )""")   #Creating table with data for clans
+#cursor.execute("DELETE FROM clans")
+#cursor.execute("INSERT INTO  clans VALUES (?, ?, ?, ?, ?, ?)", (0, 1, 2, 3, 4, 5))
+connection.commit()
+#cursor.execute("SELECT * FROM clans")
+#for x in cursor:
+#    print(x)
+#END DATABASE
+
+
+#Classes
+
+class Clan:
+
+    def __init__(self, id):
+        #Creating clan object
+        #Selecting data from database
+        data = {}
+        cursor.execute("SELECT * FROM clans WHERE clanid=?", (id,))
+        raw_data = cursor.fetchall()
+        for el in raw_data:
+            i = 0
+            for x in el:
+                data[i] = x
+                i += 1
+        self.id = data[0]
+        if data[1] == 0:
+            self.public = False
+        elif data[1] == 1:
+            self.public = True
+        self.owner = data[2]
+        self.level = data[3]
+        self.name = data[4]
+        self.castle = data[5]
+
+    #def delete(self, id)
+
+
+class User:
+
+    def __init__(self, id):
+        cursor.execute("SELECT * FROM users WHERE userid=?", (id,))
+        data = cursor.fetchone()
+        if data is None:
+            raise ValueError
+        else:
+            user_data = {}
+            i = 0
+            for x in data:
+                user_data[i] = x
+                i += 1
+            self.id = user_data[0]
+            self.name = user_data[1]
+            if user_data[2] == 0:
+                self.clan = None
+            else:
+                self.clan = Clan(user_data[2])
+            self.money = user_data[3]
+            self.attack = user_data[5]
+            self.health = user_data[6]
+            self.defend = user_data[7]
+
+    def delete(self):
+        cursor.execute("DELETE FROM users WHERE userid=?", (self.id,))
+        if self.clan != None:
+            cursor.execute("DELETE FROM clans WHERE owner=?", (self.id,))
+            cursor.execute("UPDATE users SET clan=? WHERE clan=?", (0, self.clan.id))
+        connection.commit()
+
+
+#END Classes
+
+
+class Slash(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @cog_ext.cog_slash(name="register", description="Register yourself for the economy system")
+    async def register(self, ctx: SlashContext):
+        try:
+            user = User(ctx.author.id)
+        except:
+            cursor.execute("INSERT INTO  users VALUES (?, ?, ?, ?, ?, ?, ?, ?)", (ctx.author.id, ctx.author.name, 0, 0, 0, 0, 0, 0))
+            connection.commit()
+            await ctx.send(f"Added {ctx.author.name} into database")
+        else:
+            await ctx.send("You are already in the databse", hidden=True)
+
+    @cog_ext.cog_slash(name="delete", description="Delete your data from the economy system")
+    async def delete(self, ctx: SlashContext):
+        try:
+            user = User(ctx.author.id)
+        except:
+            await ctx.send("You are not in the database", hidden=True)
+        else:
+            user.delete()
+            await ctx.send(f"Deleted {user.name}")
+
+def setup(bot):
+    bot.add_cog(Slash(bot))
--- reggin.py
+++ reggin.py
@@ -1,121 +1,9 @@
 import discord
 from discord_slash import SlashCommand, SlashContext
 from discord.ext import commands
-import sqlite3
 
 client = commands.Bot(command_prefix = 'reg:')
 slash = SlashCommand(client, sync_commands=True)
-
-#Utility functions
-def read_replace(read, type="db"):
-    #read = str(read)
-    if type != "db":
-        read=str(read)
-    if type == "db":
-        if read is None:
-            return None
-        for r in read:
-            read = r
-    elif type == "userid":
-        read = read.replace(">", "")
-        read = read.replace("<@", "")
-        read = read.replace("!", "")
-    elif type == "channelid":
-        read = read.replace("<#", "")
-        read = read.replace(">", "")
-    elif type == "roleid":
-        read = read.replace("<", "")
-        read = read.replace("@", "")
-        read = read.replace("&", "")
-        read = read.replace(">", "")
-    return read
-#End utility functions
-
-
-
-#DATABASE CREATE
-connection = sqlite3.connect("/media/pi/JONAH_3/Python/Discord_Bot/Regin/Reggin_data.db") #Opening Database
-cursor = connection.cursor()
-
-#cursor.execute("""CREATE TABLE users (
-#        userid INT, ing_name TEXT, clan INT, money INT, lastDaily TEXT, attack INT, health INT, defend INT
-#    )""")   #Creating table with data for users
-
-#cursor.execute("DROP TABLE clans")
-
-#cursor.execute("""CREATE TABLE clans (
-#        clanid INT, public INT, owner INT, level INT, name TEXT, castle INT
-#    )""")   #Creating table with data for clans
-#cursor.execute("DELETE FROM clans")
-#cursor.execute("INSERT INTO  clans VALUES (?, ?, ?, ?, ?, ?)", (0, 1, 2, 3, 4, 5))
-connection.commit()
-#cursor.execute("SELECT * FROM clans")
-#for x in cursor:
-#    print(x)
-#END DATABASE
-
-
-#Classes
-
-class Clan:
-
-    def __init__(self, id):
-        #Creating clan object
-        #Selecting data from database
-        data = {}
-        cursor.execute("SELECT * FROM clans WHERE clanid=?", (id,))
-        raw_data = cursor.fetchall()
-        for el in raw_data:
-            i = 0
-            for x in el:
-                data[i] = x
-                i += 1
-        self.id = data[0]
-        if data[1] == 0:
-            self.public = False
-        elif data[1] == 1:
-            self.public = True
-        self.owner = data[2]
-        self.level = data[3]
-        self.name = data[4]
-        self.castle = data[5]
-
-    #def delete(self, id)
-
-
-class User:
-
-    def __init__(self, id):
-        cursor.execute("SELECT * FROM users WHERE userid=?", (id,))
-        data = cursor.fetchone()
-        if data is None:
-            raise ValueError
-        else:
-            user_data = {}
-            i = 0
-            for x in data:
-                user_data[i] = x
-                i += 1
-            self.id = user_data[0]
-            self.name = user_data[1]
-            if user_data[2] == 0:
-                self.clan = None
-            else:
-                self.clan = Clan(user_data[2])
-            self.money = user_data[3]
-            self.attack = user_data[5]
-            self.health = user_data[6]
-            self.defend = user_data[7]
-
-    def delete(self):
-        cursor.execute("DELETE FROM users WHERE userid=?", (self.id,))
-        if self.clan != None:
-            cursor.execute("DELETE FROM clans WHERE owner=?", (self.id,))
-            cursor.execute("UPDATE users SET clan=? WHERE clan=?", (0, self.clan.id))
-        connection.commit()
-
-
-#END Classes
 
 
 @client.event
@@ -127,26 +15,5 @@ async def on_ready():
     embed = discord.Embed(description = f"Bot online", color=discord.Colour(0xd4af37))
     await channel.send(embed=embed)
 
-@slash.slash(name="register", description="Register yourself for the economy system")
-async def register(ctx: SlashContext):
-    try:
-        user = User(ctx.author.id)
-    except:
-        cursor.execute("INSERT INTO  users VALUES (?, ?, ?, ?, ?, ?, ?, ?)", (ctx.author.id, ctx.author.name, 0, 0, 0, 0, 0, 0))
-        connection.commit()
-        await ctx.send(f"Added {ctx.author.name} into database")
-    else:
-        await ctx.send("You are already in the databse")
-
-@slash.slash(name="delete", description="Delete your data from the economy system")
-async def delete(ctx: SlashContext):
-    try:
-        user = User(ctx.author.id)
-    except:
-        await ctx.send("You are not in the database")
-    else:
-        user.delete()
-        await ctx.send(f"Deleted {user.name}")
-
-
-client.run('Token')
+client.load_extension('economy')
+client.run('ODY4Mzg2NTk3NjE0MjU2MTc5.YPu6Cg.qKmdUQ0is4TyBP2puWKdOGSu_2k')
